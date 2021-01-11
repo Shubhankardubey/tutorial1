1a8d1a8306e888d31f655bb521c8d4bec1a18286
shubhankar <shubhankar.dubey@pristyncare.com>
2021-01-11 21:13:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,116 @@
 import React, {Component} from "react";
-import PropTypes, { element } from 'prop-types';
-import ReactDom from "react-dom";
-import Test from "./testComponent";
-import {TransitionGroup} from 'react-transition-group';
 import './App.css';
+import {CSSTransition} from 'react-transition-group';
 
 class App extends React.Component{
 
   constructor(props){
     super(props)
     this.state = {
-      items:["Advanced", "Reactjs", "Angular", "JS"]
+     showList: true,
+     highlightedHobby:false
     }
-    this.handleAdd = this.handleAdd.bind(this)
+  }
+  
+  switch = () =>{
+    this.setState(prevState=>({
+      showList: !prevState.showList
+    }))
   }
 
-  handleAdd(){
-    const newItems = this.state.items.concat([prompt("Enter Item name")])
-    this.setState({
-      items: newItems
-    })
+  listSwitch = () => {
+    this.setState(state=>({
+      highlightedHobby: !state.highlightedHobby   
+    }))
   }
 
-  handleRemove(i){
-    let newItems = this.state.items.slice()
-    newItems.splice(i,1)
-    this.setState({
-      items: newItems
-    })
-  } 
+  // submit = () => {
+  //   let score = 0
+  //   let tempArr = JSON.parse(JSON.stringify(this.state.quetionArray))
+  //   tempArr.forEach((eachQues,index)=>{
+  //     if(eachQues.multiple){
+  //       let userSelectedAns = eachQues.answerArray.filter(eachAns => eachAns["userSelected"]).map(eachAns=>eachAns["label"])
+  //       if(userSelectedAns && userSelectedAns.length > 0){
+  //         tempArr[index]["questionAttempted"] = true
+  //         console.log("userSelectedAns",userSelectedAns)
+  //         console.log("eachQu", eachQues["correctAnswer"])
+  //         if(JSON.stringify(userSelectedAns) === JSON.stringify(eachQues["correctAnswer"])){
+  //           score += 10
+  //         }
+  //       }
+  //     }else{
+  //       let userSelectedAns = eachQues.answerArray.filter(eachAns => eachAns["userSelected"])
+  //       if(userSelectedAns && userSelectedAns.length > 0){
+  //         tempArr[index]["questionAttempted"] = true
+  //       if(userSelectedAns[0]['label'] === eachQues["correctAnswer"]){
+  //         score += 10
+  //       }
+  //     }
+  //     else{
+  //       console.log("1", index)
+  //     }
+  //     }
+  //   })
+  //   this.setState({
+  //     quetionArray: tempArr,
+  //     isSubmit: true
+  //   })
+  // }
+
+  // selectAnswer = (quesIndex, ansIndex) => {
+  //   let tempArr = JSON.parse(JSON.stringify(this.state.quetionArray))
+  //   tempArr[quesIndex]["answerArray"][ansIndex]["userSelected"] = true
+  //   this.setState({
+  //     quetionArray:tempArr
+  //   })
+  // }
+
 
   render(){
-    const items = this.state.items.map((item, i)=>{
-      <div key={i} onClick={()=>this.handleRemove(i)}>
-        {item}
-        </div>
-    })
     return (
-      <div>
-        <button type="button" onClick={this.handleAdd}></button>
-        <TransitionGroup 
-        transitionName="example"
-        transitionEnterTimeout={800}
-        transitionLeaveTimeout={600}
-        >
-          {items}
-          </TransitionGroup>
-        </div>      
+      // <div style={{padding: "40px"}}>
+      //     {this.state.quetionArray.map((eachQues, quesIndex)=>{
+      //       return (
+      //         <>
+      //         <div style={{padding: "40px"}}>
+      //           {eachQues.questionLabel} {eachQues.multiple ? "Multiple" : null} 
+      //           {this.state.isSubmit && <>
+      //             {eachQues.questionAttempted ? null : "Not Attempted"}
+      //           </>}
+      //           </div>
+      //         {eachQues.answerArray.map((eachAns, ansIndex)=>{
+      //           return (
+      //             <>
+      //             <input type="checkbox" checked={eachAns.userSelected} onChange={()=>this.selectAnswer(quesIndex, ansIndex)} /> {eachAns.label}
+      //             </>
+      //           )
+      //         })}
+      //         </>
+      //       )
+      //     })}
+      //     <br/>
+      //     <button type="button" onClick={()=>this.submit()}> Hello</button>
+      //   </div>  
+      <div className="container">
+        <button className="display" onClick={this.switch}>
+          Hello
+          </button>
+          <CSSTransition
+          in={this.state.showList}
+          timeout={400}
+          classNames="list-transition"
+          unmountOnExit
+          appear
+          onEntered={this.listSwitch}
+          onExit={this.listSwitch}
+          >
+            <div className="list-body">
+              <ul className="list">
+                hello
+                </ul>
+              </div>
+          </CSSTransition>
+        </div>    
     )
   }
 }
