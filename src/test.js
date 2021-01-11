21b7c967d280f885515bd16cefd2c6c5256e0767
shubhankar <shubhankar.dubey@pristyncare.com>
2021-01-11 21:13:59 +0530

    changes

---
+++ src/test.js
@@ -0,0 +1,24 @@
+let data = [
+    {
+      "label":"Shubh",
+      userSelected: false
+    },
+    {
+      "label":"Shubhas",
+      userSelected: false
+    },
+    {
+      "label":"Shubhas",
+      userSelected: false
+    },
+    {
+      "label":"Shubhankar",
+      userSelected: false
+    }
+  ]
+
+  let newData = data.map(x=>x["label"])
+
+[
+    "Shubh","Shubhas", "Shubhas", "Shubhankar"
+]
