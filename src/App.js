c4ed96750a5a3b640803df117caa61497d283757
Shubhankardubey <shubhankar.dubey@pristyncare.com>
2021-01-17 21:07:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
-// import {CSSTransition} from 'react-transition-group';
-import {HashRouter, NavLink, Route} from 'react-router-dom';
-import {Link} from 'react-router-dom';
-import About from "./About"
-import Test from "./testComponent"
-// const {Provider, Consumer} = React.createContext(defaultValue)
 
-class App extends React.Component{
+function useOffline(){
+  const [isOffline, setIsOffline] = useState(false)
 
-  constructor(props){
-    super(props)
-    this.state={}
-  }
-
-
-  componentDidMount(){
-    this.interval = setInterval(()=>this.setState({time: new Date().toISOString()}), 1000) 
-  }
-
-  componentWillMount(){
-    clearInterval(this.interval)
-  }
-
-  render(){
-    return <div>
-      <input type="file" ref={input=>this.inputEleRef = input} style={{display:"none"}}/>
-      <button type="buton" onClick={()=>this.inputEleRef.click()}>Hello Please Upload File</button>
-    </div>
-  }
+  useEffect(()=>{
+    window.addEventListener("offline", setIsOffline(true))
+    window.addEventListener("online", setIsOffline(false))
+    
+  },[])
+  return isOffline
 }
 
-export default App;
+export default function App(){
+  const isOffline = useOffline()
+  return (
+    <div>
+    {isOffline ? <div>Sorry You are offline</div> : <div>You are online</div>} 
+    </div>
+  )
+};
--- src/index.js
+++ src/index.js
@@ -7,17 +7,81 @@ import About from './About';
 import NotFound from './notfound';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, HashRouter} from 'react-router-dom';
+import { func } from 'prop-types';
 
-let value = false
 
-const routing = (
-  <BrowserRouter>
-<App/>
-  </BrowserRouter>
-)
+// const routing = (
+//   <BrowserRouter>
+// <App/>
+//   </BrowserRouter>
+// )
+
+// class ErrorBoundary extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.state = {error:null, errorInfo:null}
+//   }
+
+//   componentDidCatch(error, errorInfo){
+//     this.setState({
+//       error: error,
+//       errorInfo: errorInfo
+//     })
+//     // api call 
+//   }
+
+//   render(){
+//     if(this.state.errorInfo){
+//       return (
+//         <div>
+//           <h2> Something Went Wrong</h2>
+//           <details style={{whiteSpace:"pre-wrap"}}>
+//             {this.state.error && this.state.error.toString()}
+//           </details>
+//           <br/>
+//           {this.state.errorInfo.componentStack}
+//           </div>
+//       )
+//     }
+//     return this.props.children
+//   }
+
+// }
+
+// class BuggyCounter extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.state = {counter:0}
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+
+//   handleClick(){
+//     this.setState(({counter})=>({
+//       counter:counter+1
+//     }))
+//   }
+
+//   render(){
+//     if(this.state.counter === 5){
+//       throw new Error('I am Crashed')
+//     }
+//     return <h1 onClick={this.handleClick}>{this.state.counter}</h1>
+//   }
+// }
+
+// function App(){
+//   return (
+//     <div>
+//       <p> Hello there</p>
+//       <ErrorBoundary>
+//         <BuggyCounter />
+//       </ErrorBoundary>
+//       </div>
+//   )
+// }
 
 ReactDOM.render(
-  routing,
+  <App/>,
   document.getElementById('root')
 );
 
